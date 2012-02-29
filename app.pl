de0b2fefce11886138ac8d1599e09a6596c626cf
Kang-min Liu <gugod@gugod.org>
2012-02-29 08:20:53 +0800

    dynamically generate bookmarklet js based on HOST

--- app.pl
+++ app.pl
@@ -15,9 +15,11 @@ my $download_worker = AnyEvent::Worker->new(
 
 get '/b' => sub {
     my $self = shift;
+    my $host = $self->req->headers->host;
+
     $self->render_text(<<"BOOKMARKLET");
 <html><body>
-<a href='javascript:(function(){var x=document.createElement("script");var u=encodeURIComponent(location.href);x.setAttribute("id","youtube-downloader");x.setAttribute("src","http://localhost:3000/dl.js?url="+u);document.body.appendChild(x);})();'>youtube-dl</a>
+<a href='javascript:(function(){var x=document.createElement("script");var u=encodeURIComponent(location.href);x.setAttribute("id","youtube-downloader");x.setAttribute("src","http://${host}/dl.js?url="+u);document.body.appendChild(x);})();'>youtube-dl</a>
 </body></html>
 BOOKMARKLET
 };
@@ -28,6 +30,5 @@ get '/dl.js' => sub {
     $download_worker->do($youtube_url, sub {});
     $self->render_text('document.body.removeChild(document.getElementById("youtube-downloader"))');
 };
-
 
 app->start;
